f1579ce5602e15f23030c871f3d4d78b29ca2843
selam <selam0315@gmail.com>
2019-06-21 13:15:54 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace employeesInCompany
+{
+       class Program
+    {
+        static void Main(string[] args)
+        {
+// Create an instance of a company. Name it whatever you like.
+
+        }
+    }
+}
