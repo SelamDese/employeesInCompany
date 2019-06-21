2b5502affcd80a5bf12cbb92aa4be58bc42897e7
selam <selam0315@gmail.com>
2019-06-21 13:20:42 -0500

    employees properties

--- Program.cs
+++ Program.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 
 namespace employeesInCompany
 {
-       class Program
-    {
-        static void Main(string[] args)
-        {
+
+// Using C# classes, you need to create custom types to represent an Employee and a Company. 
+// Then you will create some employees, hire them into the company and then display 
+// a simple report showing the employee names and their titles.
+    class Employee
+            {
+// Create a custom type for Employee. An employee has the following properties.
+                public string FirstName { get; set; }
+                public string LastName { get; set; }
+                public string Title { get; set; }
+                public DateTime StartDate {get; set; }
+
+            }
+    class Program
+            {
+               static void Main(string[] args)
+            {
 // Create an instance of a company. Name it whatever you like.
 
         }
